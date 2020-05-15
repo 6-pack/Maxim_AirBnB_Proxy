618faed1cdf76d740cf4197419d20944ae63986f
Maxim Rietveld <maxrietveld@gmail.com>
2020-05-15 09:36:02 -0700

    proxy working locally, ready for deployment

--- server.js
+++ server.js
@@ -9,27 +9,27 @@ app.listen(PORT, () => { console.log(`Express server listening on port#${PORT}`)
 app.use(express.static('.'));
 
 app.get('/rooms/:roomID/reviews', (req, res) => {
-  axios.get('http://localhost:3001/rooms/2/reviews')
+  axios.get('http://18.220.252.131:3001/rooms/2/reviews')
     .then(result => res.status(200).send(result.data))
     .catch(err => console.log(err));
 });
 app.get('/rooms/:roomID/reviews/:phrase', (req, res) => {
-  axios.get(`http://localhost:3001/rooms/2/reviews/${req.params.phrase}`)
+  axios.get(`http://18.220.252.131:3001/rooms/2/reviews/${req.params.phrase}`)
     .then(result => res.status(200).send(result.data))
     .catch(err => console.log(err));
 });
 app.get('/image', (req, res) => {
-  axios.get(`http://localhost:3003/image`)
+  axios.get(`http://13.57.183.102:3003/carousel`)
     .then(result => res.status(200).send(result.data))
     .catch(err => console.log(err));
 });
 app.get('/rooms', (req, res) => {
-  axios.get(`http://localhost:3004/rooms`)
+  axios.get(`http://3.101.29.123:3004/rooms`)
     .then(result => res.status(200).send(result.data))
     .catch(err => console.log(err));
 });
 app.get('/data', (req, res) => {
-  axios.get(`http://localhost:3002/data`)
+  axios.get(`http://18.217.34.96:3002/data`)
     .then(result => res.status(200).send(result.data))
     .catch(err => console.log(err));
 });
